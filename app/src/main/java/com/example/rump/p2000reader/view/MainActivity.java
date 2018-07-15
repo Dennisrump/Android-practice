13a1a0e3e940e1f478412b3afc787d38347f2bca
Dennis Rump <dennisrump10@hotmail.com>
2018-07-15 19:07:53 +0200

    Fragment manager

--- app/src/main/java/com/example/rump/p2000reader/view/MainActivity.java
+++ app/src/main/java/com/example/rump/p2000reader/view/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.rump.p2000reader.view;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.rump.p2000reader.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         replaceFragment(new DashboardFragment());
+        navigationListener();
+    }
+
+    private void navigationListener() {
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_main_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()) {
+                    case R.id.navigation_dashboard:
+                        replaceFragment(new DashboardFragment());
+                        break;
+                    case R.id.navigation_accidents:
+                        replaceFragment(new AccidentsFragment());
+                        break;
+                    case R.id.navigation_map:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     private void replaceFragment(Fragment fragment) {
